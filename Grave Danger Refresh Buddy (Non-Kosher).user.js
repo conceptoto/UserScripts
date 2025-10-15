6b84504693ab6c2474adb8ac961587524a5e43eb
José Ignacio Alegria <50333515+conceptoto@users.noreply.github.com>
2025-10-14 21:53:50 -0300

    Update Grave Danger Refresh Buddy (Non-Kosher).user.js

--- Grave Danger Refresh Buddy (Non-Kosher).user.js
+++ Grave Danger Refresh Buddy (Non-Kosher).user.js
@@ -8,7 +8,6 @@
 (function() {
     const waitTime = 10 * 60 * 1000; //600 THOUSAND milliseconds (10 minutes)
     const fuzzedTime = waitTime + Math.floor(Math.random() * 20 * 1000) //plus a random time between 0 and 20 seconds
-    console.log(`${fuzzedTime/1000} seconds left to refresh`)
     const now = Date.now();
     console.log(`next rf by ${new Date(fuzzedTime + now).toLocaleTimeString()}`)
     setTimeout(() => {
